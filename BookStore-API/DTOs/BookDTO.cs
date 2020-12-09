5ac33ae9804b3d76178ce27d85d179688ab6ed93
johhny99 <tonanhminh1119@gmail.com>
2020-12-09 15:17:30 +0700

    Created Data Class and DTOs. ALso included automapper and change app setting file.

---
+++ BookStore-API/DTOs/BookDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore_API.DTOs
+{
+    public class BookDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int? Year { get; set; }
+        public string Isbn { get; set; }
+        public string Summary { get; set; }
+        public double? Image { get; set; }
+        public int? AuthorId { get; set; }
+        public virtual AuthorDTO Author { get; set; }
+    }
+}
---
+++ BookStore-API/Data/Book.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookStore_API.Data
+{
+    [Table("Books")]
+    public partial class Book
+    {
+        public int Id { get; set; }
+        public string Title {get; set; }
+        public int? Year { get; set; }
+        public string Isbn { get; set; }
+        public string Summary { get; set; }
+        public double? Image { get; set; }
+        public int? AuthorId { get; set; }
+        public virtual Author Author { get; set; }
+    }
+}
