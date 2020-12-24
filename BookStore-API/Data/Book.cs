1e38a053c4558364b90dd86fc12a212c4c8f3a47
johhny99 <tonanhminh1119@gmail.com>
2020-12-24 16:12:25 +0700

    Added Book CRUD Operation, Fixed Book API DTO issues and added Toast notification

--- BookStore-API/Data/Book.cs
+++ BookStore-API/Data/Book.cs
@@ -36,6 +36,8 @@ namespace BookStore_API.Data
         [Required]
         public string Title { get; set; }
         public int? Year { get; set; }
+        [Required]
+        public string Isbn { get; set; }
         [StringLength(500)]
         public string Summary { get; set; }
         public string Image { get; set; }
